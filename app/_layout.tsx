4ef2e664c8880f0c23f49dc655f964f79e43d407
Mateusz <matiro28@gmail.com>
2026-05-14 15:55:54 +0200

    [#9] Hide header on onboarding screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,6 +11,7 @@ function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="onboarding" options={{ headerShown: false }} />
     </Stack>
   );
 }
